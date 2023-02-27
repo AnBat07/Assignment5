4dbee5300316aab2d43773c16e8a59bc2425b8ca
AnBat07 <anitabatishev@gmail.com>
2023-02-27 11:08:14 -0800

    added files

---
+++ fancifymytext.js
@@ -0,0 +1,30 @@
+function bigger(){
+     //alert("Hello, world!") //used during the alert box when click bigger
+     document.getElementById("Text").style.fontSize='4em';
+}
+
+function textChange() {
+    //checks to see if the fancy button was clicked
+    //if so then it will bold the words in the text box and underline them and change to blue color
+    if(document.getElementById('fancyRadioButton').checked == true){
+        document.getElementById("Text").style.fontWeight= "bold";
+        document.getElementById("Text").style.color= "blue";
+        document.getElementById("Text").style.textDecoration= "underline";
+    }
+
+    //checks to see if boring button is clicked
+    //if so then all fonts and changes will go back to normal
+    if(document.getElementById('boringRadioButton').checked == true){
+        document.getElementById("Text").style.fontWeight= "normal";
+        document.getElementById("Text").style.color="black";
+        document.getElementById("Text").style.textDecoration= "none";
+    }
+}
+
+function mooButton(){
+    var str = document.getElementById("text").value;
+    var upper= text.toUpperCase();
+    var parts= upper.split(".");
+    txt= parts.join("-Moo");
+    document.getElementById("text").value= txt;
+}
